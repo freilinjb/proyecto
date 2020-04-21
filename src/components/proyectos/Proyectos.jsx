ebc6d960e66ad3f69664ff42ba807cc652a94a42
freilinjb <freilinjb@gmail.com>
2020-04-21 07:09:06 -0400

    agrergando Componente Barra

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sidebar from '../layout/Sidebar';
+import Barra from '../layout/Barra';
 
 const Proyectos = () => {
     return ( 
@@ -10,7 +11,7 @@ const Proyectos = () => {
             <div className="seccion-principal">
                 <main>
                     <div className="contenedor-tareas">
-
+                        <Barra/>
                     </div>
                 </main>
             </div>
