3799fe18c23991dff42ab812b040480209b5e535
freilinjb <freilinjb@gmail.com>
2020-04-22 23:09:43 -0400

    mostrando las tareas del state en el DOM filtradas por proyecto seleccionado = probado

--- src/components/tareas/LIstadoTareas.jsx
+++ src/components/tareas/LIstadoTareas.jsx
@@ -2,6 +2,7 @@ import React, {Fragment, useContext} from 'react';
 import Tarea from '../tareas/Tarea';
 
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 
 const ListadoTareas = () => {
@@ -9,6 +10,10 @@ const ListadoTareas = () => {
     //Extraer proyectos del state inicial
     const proyectosContext = useContext(proyectoContext);
     const { proyecto, eliminarProyecto } = proyectosContext;
+
+    //Obtener las tareas del proyecto
+    const tareasContext = useContext(tareaContext);
+    const { tareasproyecto } = tareasContext;
  
     //Si no hay un proyecto seleccionado    
     if(!proyecto) return <h2>Selecciona un proyecto</h2>
@@ -16,8 +21,6 @@ const ListadoTareas = () => {
     //Array destructuring para extraer el proyecto actual
     const [proyectoActual]= proyecto;
 
-    //Prueba 
-    const tareasProyecto = []
 
     //Eliminar proyecto
     const onClickEliminar = () => {
@@ -29,9 +32,9 @@ const ListadoTareas = () => {
 
             <ul className="listado-tareas">
                 {/* Ternario */}
-                {tareasProyecto.length === 0
+                {tareasproyecto.length === 0
                     ? (<li className="tarea"><p>No hay tareas</p></li>)
-                    : tareasProyecto.map(tarea => (
+                    : tareasproyecto.map(tarea => (
                         <Tarea tarea={tarea}/>
                     ))
                 }
